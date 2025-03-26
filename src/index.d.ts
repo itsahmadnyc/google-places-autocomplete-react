37e08b64c030ad3ce0a7fee94427298e2d06c6f5
Muhammad Ahmad <its_ahmad_nyc@yahoo.com>
2025-03-26 09:36:25 +0500

    Typescript Support Changes

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 declare module "google-places-autocomplete-react" {
-    import React from "react";
+    import React, { CSSProperties } from "react";
 
     export interface PlaceDetails {
         description: string;
@@ -11,6 +11,12 @@ declare module "google-places-autocomplete-react" {
         apiKey: string;
         onPlaceSelect: (place: PlaceDetails) => void;
         placeholder?: string;
+        inputClassName?: string;
+        suggestionsClassName?: string;
+        suggestionItemClassName?: string;
+        inputStyle?: CSSProperties;
+        suggestionsStyle?: CSSProperties;
+        suggestionItemStyle?: CSSProperties;
     }
 
     const GooglePlacesAutocomplete: React.FC<GooglePlacesAutocompleteProps>;
